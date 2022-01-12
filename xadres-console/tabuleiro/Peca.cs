99414fc1a16b22e33665cb94f67242757e216c72
Gerson Bernardo <gbds25@gmail.com>
2022-01-12 09:27:14 -0300

    Começando a implementar umm Método que imprime o tabuleiro

--- xadres-console/tabuleiro/Peca.cs
+++ xadres-console/tabuleiro/Peca.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace xadres_console.tabuleiro
+﻿namespace tabuleiro
 {
     class Peca
     {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get;protected set; }
+        public Tabuleiro tabuleiro { get;protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tabuleiro = tabuleiro;
+            this.qteMovimentos = 0;
+        }
     }
 }
